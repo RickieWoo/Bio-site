2e9d12fa2366f23fc41773007384d453b53d7456
rick <kylin792884522@outlook.com>
2017-05-03 10:45:51 +0800

    first commit

---
+++ js/maintain.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("#img1").hover(function(){
+    $(".m-img").css("background-image","url(./img/img.jpg)");
+  });
+  $("#img2").hover(function(){
+    $(".m-img").css("background-image","url(./img/img2.jpg)");
+  });
+  $("#img3").hover(function(){
+    $(".m-img").css("background-image","url(./img/img3.jpg)");
+  });
+});
+	
