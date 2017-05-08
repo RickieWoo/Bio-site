39f3a8cbb78626df728efb2df550366e7eaef49c
rick <kylin792884522@outlook.com>
2017-05-09 05:42:30 +1400

    images changed

--- js/maintain.js
+++ js/maintain.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   $("#img1").hover(function(){
-    $(".m-img").css("background-image","url(./img/img.jpg)");
+    $(".m-img").css("background-image","url(http://life.hust.edu.cn:8181/u/cms/www/201701/24130323u45r.JPG)");
   });
   $("#img2").hover(function(){
-    $(".m-img").css("background-image","url(./img/img2.jpg)");
+    $(".m-img").css("background-image","url(http://life.hust.edu.cn:8181/u/cms/www/201611/01142031ikh1.jpg)");
   });
   $("#img3").hover(function(){
-    $(".m-img").css("background-image","url(./img/img3.jpg)");
+    $(".m-img").css("background-image","url(http://life.hust.edu.cn:8181/u/cms/www/201611/21155750piqp.JPG)");
   });
 });
 	
