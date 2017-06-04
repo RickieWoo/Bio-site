d0a1c96dd7e3a4b2a2253bb96a0f856d660e9519
rick <kylin792884522@outlook.com>
2017-06-04 23:29:07 +1400

    the finial version maybe, without link-direction

--- js/maintain.js
+++ js/maintain.js
@@ -1,12 +1,43 @@
 $(document).ready(function(){
+  // document.getElementById("img1").hover();
+
   $("#img1").hover(function(){
-    $(".m-img").css("background-image","url(http://life.hust.edu.cn:8181/u/cms/www/201701/24130323u45r.JPG)");
+    $(".m-img").css("background-image","url(./img/1.jpg)");
   });
   $("#img2").hover(function(){
-    $(".m-img").css("background-image","url(http://life.hust.edu.cn:8181/u/cms/www/201611/01142031ikh1.jpg)");
+    $(".m-img").css("background-image","url(./img/2.jpg)");
   });
   $("#img3").hover(function(){
-    $(".m-img").css("background-image","url(http://life.hust.edu.cn:8181/u/cms/www/201611/21155750piqp.JPG)");
+    $(".m-img").css("background-image","url(./img/3.jpg)");
   });
+  $("#img4").hover(function(){
+    $(".m-img").css("background-image","url(./img/4.jpg)");
+  });
+  $("#img5").hover(function(){
+    $(".m-img").css("background-image","url(./img/5.jpg)");
+  });
+
+
 });
-	
+
+var mySwiper = new Swiper('.swiper-container', {
+  autoplay: 3000,//可选选项，自动滑动
+  pagination : '.swiper-pagination',
+  paginationClickable :true,
+  // loop: true
+});
+// function check_number(){
+//   var value = $("#login").value;
+//   function check_validate1(value){
+//     var reg = /^[UM]+\d+$/;
+//     if( value.constructor === String ){
+//     var re = value.match( reg );
+    
+//   }
+//     $("#notice").text = "wrong";
+//   }
+// }
+function isNumber(){
+ isNum = /^[0-9]*$/;
+ alert(isNum.test($("#login").value));
+}
